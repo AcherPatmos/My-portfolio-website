c4944b7d1f079398cbb81f67b48d62e57c131dc5
Patmos <a.mpakaniye@alustudent.com>
2026-07-19 08:41:59 +0400

    added in basic message validation to the contact form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+const nameInput = document.getElementById("name_input");
+const emailInput = document.getElementById("email_input");
+const message = document.getElementById("Textarea");
+
 document.getElementById("button").addEventListener("click", function () {
-  window.alert("Your message has been sent!");
+  if (
+    nameInput.value !== "" &&
+    emailInput.value !== "" &&
+    message.value !== ""
+  ) {
+    window.alert("Your message has been sent!");
+  } else if (
+    nameInput.value === "" &&
+    emailInput.value === "" &&
+    message.value === ""
+  ) {
+    alert("Please fill all the empty placeholders");
+  } else if (emailInput.value === "" && message.value === "") {
+    alert(
+      `${nameInput.value}, please fill in the rest of the empty placeholders`,
+    );
+  } else if (message.value === "") {
+    alert(`${nameInput.value}, please write your intended message`);
+  } else if (nameInput.value === "") {
+    alert("Please enter your name");
+  } else if (emailInput.value === "") {
+    alert(`${nameInput.value}, please enter your email address`);
+  }
 });
