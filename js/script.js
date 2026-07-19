4704da5654796fce29d0a13ae2a4a4f9a24045f0
Patmos <a.mpakaniye@alustudent.com>
2026-07-19 08:15:41 +0400

    added in a window alert once a user clicks send message to confirm the message was sent successfully

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-
+document.getElementById("button").addEventListener("click", function () {
+  window.alert("Your message has been sent!");
 });
